4745a2e448440d5b596c0f7e9a99a09e6cbcb0c3
Firdaws-b <firdaws.bouzeghaya@mail.concordia.ca>
2025-10-25 21:45:27 -0400

    code cleanup

--- src/core/security.py
+++ src/core/security.py
@@ -1,5 +1,3 @@
-from datetime import timedelta, datetime
-
 from passlib.context import CryptContext
 import jwt, os
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
@@ -14,14 +12,6 @@ def get_password_hash(password):
 def create_token(data: dict):
     return jwt.encode(data, SECRET_KEY, algorithm="HS256")
 
-# def create_access_token(data: dict, expires_delta: timedelta = None):
-#     to_encode = data.copy()
-#     if expires_delta:
-#         expire = datetime.utcnow() + expires_delta
-#
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
-#     return encoded_jwt
 
 
 
