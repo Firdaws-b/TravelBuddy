039029f45eac4dda3c78e39d15e2936629a57e7f
Firdaws-b <firdaws.bouzeghaya@mail.concordia.ca>
2025-10-25 21:53:37 -0400

    Bug fix: ModuleNotFoundError

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import FastAPI
 from config.settings import settings
 from src.routers import hotel_router, auth_router, user_router
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 app = FastAPI(title="TravelBuddy API", version="1.0.0")
 # Include router with base prefix (e.g., /api/v1)
 app.include_router(hotel_router.router, prefix=settings.BASE_ROUTE)
