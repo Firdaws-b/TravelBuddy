9ed025e088a1d6aa742ec557ac8cffcaa386924b
Tra1307 <terivwode@gmail.com>
2025-11-23 18:06:52 -0500

    added recommendations

--- config/databse.py
+++ config/databse.py
@@ -6,6 +6,7 @@ client = MongoClient(settings.MONGO_URI, tlsCAFile=certifi.where())
 db = client["TravelBuddy"]
 users_collection = db["Users"]
 destinations_collection = db["Destinations"]
+recommendations_collection = db["Recommendations"]
 trips_collection = db["Trips"]
 hotels_collection = db["hotels"]
 hotel_bookings_collection = db["hotel_bookings"] 
